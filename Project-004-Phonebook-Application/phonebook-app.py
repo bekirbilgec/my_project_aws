ab8c813e9b8b4029ca7cc6c44730fda1eb6f63bb
bekirbilgec <bekirbilgec42@gmail.com>
2022-10-04 11:26:59 +0300

    update

--- Project-004-Phonebook-Application/phonebook-app.py
+++ Project-004-Phonebook-Application/phonebook-app.py
@@ -6,16 +6,16 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # This "/home/ec2-user/dbserver.endpoint" file has to be created from cloudformation template and it has RDS endpoint
-db_endpoint = open("/home/ec2-user/dbserver.endpoint", 'r', encoding='UTF-8') 
+# db_endpoint = open("/home/ec2-user/dbserver.endpoint", 'r', encoding='UTF-8') 
 
 # Configure mysql database
 
-app.config['MYSQL_DATABASE_HOST'] = db_endpoint.readline().strip()
+app.config['MYSQL_DATABASE_HOST'] = 'bekir-db-myproject.c1avrzrdyjjt.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Please_Enter_Your_DB_Password'
-app.config['MYSQL_DATABASE_DB'] = 'clarusway_phonebook'
+app.config['MYSQL_DATABASE_PASSWORD'] = '42580635'
+app.config['MYSQL_DATABASE_DB'] = 'phonebookbekir'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-db_endpoint.close()
+# db_endpoint.close()
 mysql = MySQL()
 mysql.init_app(app) 
 connection = mysql.connect()
@@ -26,7 +26,7 @@ cursor = connection.cursor()
 
 def init_phonebook_db():
     phonebook_table = """
-    CREATE TABLE IF NOT EXISTS clarusway_phonebook.phonebook(
+    CREATE TABLE IF NOT EXISTS phonebookbekir.phonebook(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     number VARCHAR(100) NOT NULL,
